b371d199abd115a2a10b4f272a02664de4d6d4d5
Mike Evron <evronm@gmail.com>
2022-03-25 14:52:57 -0700

    some more tests

--- src/test/Attestations.t.sol
+++ src/test/Attestations.t.sol
@@ -4,9 +4,14 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "src/Attestations.sol";
 
+interface CheatCodes {
+    function prank(address) external;
+      function expectRevert(bytes calldata) external;
+}
 
 contract FreeFormAttestationTest is DSTest {
   FreeForm freeform;
+  CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
   function setUp() public {
     freeform = new FreeForm("freeformtest");
   }
@@ -22,6 +27,16 @@ contract FreeFormAttestationTest is DSTest {
     assertEq(freeform.attestees().length,0);
   }
 
+  function testAttestation() public {
+    freeform.attest(address(1));
+    freeform.attest(address(2));
+    freeform.attest(address(3));
+    cheats.expectRevert(bytes("Attestation already exists."));
+    freeform.attest(address(3));
+
+    assertEq(freeform.attestors().length,1);
+    assertEq(freeform.attestees().length,3);
+  }
   //This needs to go in ds-test
   function compareStrings(string memory a, string memory b) public pure returns (bool) {
     return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
