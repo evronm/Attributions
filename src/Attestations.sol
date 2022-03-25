6304764644dfcad34f27683a50bc9b9a9e1045eb
Mike Evron <evronm@gmail.com>
2022-03-25 12:37:37 -0700

    added attestors and attestees lists.  Still needs tons of test coverage

--- src/Attestations.sol
+++ src/Attestations.sol
@@ -3,52 +3,70 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 abstract contract AttestationList {
-  string private _attestation;
-  mapping (address => address[]) public attestations_by;
-  mapping (address => address[]) public attestations_about;
+  mapping (address => address[]) public _attestations_by;
+  mapping (address => address[]) public _attestations_about;
+  address[] public _attestors;
+  address[] public _attestees;
+
+  function attestations_by (address attestor) public view returns (address[] memory){
+    return _attestations_by[attestor];
+  }
+
+  function attestations_about (address attestee) public view returns (address[] memory){
+    return _attestations_about[attestee];
+  }
+  function attestors () public view returns (address[] memory){
+    return _attestors;
+  }
+  function attestees () public view returns (address[] memory){
+    return _attestees;
+  }
 
   function attest(address about_addr) public {
     require (find_redundant(about_addr) == false, "Attestation already exists.");
-    attestations_by[msg.sender].push(about_addr);
-    attestations_about[about_addr].push(msg.sender);
+    if (attestations_by(msg.sender).length==0) {
+      _attestors.push(msg.sender);
+    }
+    if (attestations_about(about_addr).length==0) {
+      _attestees.push(msg.sender);
+    }
+    _attestations_by[msg.sender].push(about_addr);
+    _attestations_about[about_addr].push(msg.sender);
   }
 
   function find_redundant (address about_addr) internal view returns (bool){
-      address[] memory attestations_by_sender=attestations_by[msg.sender];
-      for (uint i=0; i < attestations_by_sender.length; i++) {
-          if (attestations_by_sender[i] == about_addr) {
-              return true;
-          }
-      }
-      return false;
+    address[] memory attestations_by_sender=_attestations_by[msg.sender];
+    for (uint i=0; i < attestations_by_sender.length; i++) {
+        if (attestations_by_sender[i] == about_addr) {
+            return true;
+        }
+    }
+    return false;
   }
 
-  function attestation () public view virtual returns (string memory) {
-    return  _attestation;
-}
 }
 contract FreeForm is AttestationList { 
   string private _attestation;
   constructor(string memory attestation_) {
     _attestation=attestation_;
   }
-  function attestation () public view override returns (string memory) {
+  function attestation () public view returns (string memory) {
     return  _attestation;
   }
 }
 
 contract Attendance is AttestationList {
-    struct event_details {
-        string name;
-        string place;
-        string date;
-    }
-    event_details private _attestation;
-    constructor (event_details memory attestation_) {
-        _attestation=attestation_;
-    }
+  struct event_details {
+    string name;
+    string place;
+    string date;
+  }
+  event_details private _attestation;
+  constructor (event_details memory attestation_) {
+    _attestation=attestation_;
+  }
 
-    function attestation () public view override returns (string memory) {
-        return  string(string.concat("name: ", bytes(_attestation.name), "\n", "place: ", bytes(_attestation.place),"\n", "date:", bytes(_attestation.date)));
-    }
+  function attestation () public view returns (string memory) {
+    return  string(string.concat("name: ", bytes(_attestation.name), "\n", "place: ", bytes(_attestation.place),"\n", "date:", bytes(_attestation.date)));
+  }
 }
--- src/test/Attestations.t.sol
+++ src/test/Attestations.t.sol
@@ -4,21 +4,25 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "src/Attestations.sol";
 
-contract TestFreeForm is FreeForm("freeformtest") {
-}
 
-contract AttestationTest is DSTest {
+contract FreeFormAttestationTest is DSTest {
   FreeForm freeform;
   function setUp() public {
     freeform = new FreeForm("freeformtest");
   }
 
-  function testExample() public {
-    freeform = new FreeForm("freeformtest");
-    emit log (freeform.attestation());
+  function testAttestationString() public {
     assertTrue(compareStrings("freeformtest",freeform.attestation()));
   }
 
+  function testEverythingEmpty() public {
+    assertEq(freeform.attestations_by(address(1)).length,0);
+    assertEq(freeform.attestations_about(address(1)).length,0);
+    assertEq(freeform.attestors().length,0);
+    assertEq(freeform.attestees().length,0);
+  }
+
+  //This needs to go in ds-test
   function compareStrings(string memory a, string memory b) public pure returns (bool) {
     return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
   }
