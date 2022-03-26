0d5390325d6351dfea583437de34c15aacea66f8
Mike Evron <evronm@gmail.com>
2022-03-26 11:46:45 -0700

    fair test coverage for free form attestations

--- src/test/Attestations.t.sol
+++ src/test/Attestations.t.sol
@@ -6,7 +6,9 @@ import "src/Attestations.sol";
 
 interface CheatCodes {
     function prank(address) external;
-      function expectRevert(bytes calldata) external;
+    function expectRevert(bytes calldata) external;
+    function startPrank(address) external;
+    function stopPrank() external;
 }
 
 contract FreeFormAttestationTest is DSTest {
@@ -28,14 +30,30 @@ contract FreeFormAttestationTest is DSTest {
   }
 
   function testAttestation() public {
+    cheats.startPrank(address(10));
     freeform.attest(address(1));
     freeform.attest(address(2));
     freeform.attest(address(3));
     cheats.expectRevert(bytes("Attestation already exists."));
     freeform.attest(address(3));
+    cheats.stopPrank();
 
     assertEq(freeform.attestors().length,1);
     assertEq(freeform.attestees().length,3);
+    assertEq(freeform.attestations_about(address(1)).length,1);
+
+    assertEq(freeform.attestations_by(address(10)).length,3);
+
+    cheats.startPrank(address(1));
+    freeform.attest(address(3));
+    freeform.attest(address(4));
+    cheats.expectRevert(bytes("Attestation already exists."));
+    freeform.attest(address(3));
+    cheats.stopPrank();
+
+    assertEq(freeform.attestors().length,2);
+    assertEq(freeform.attestees().length,4);
+    assertEq(freeform.attestations_about(address(3)).length,2);
   }
   //This needs to go in ds-test
   function compareStrings(string memory a, string memory b) public pure returns (bool) {
