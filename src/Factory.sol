027cb5c6307b35dde16d2b174158cf5372f8211c
Mike Evron <evronm@gmail.com>
2022-04-11 23:10:19 -0700

    removed debugging code from factory

--- src/Factory.sol
+++ src/Factory.sol
@@ -8,14 +8,9 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract Factory is Initializable {
   address private base_registry;
-  Registry private _br;
   function init() public initializer returns (Factory) {
-    _br=new Registry().init("");
-    base_registry=address(_br);
+    base_registry=address(new Registry().init(""));
     return this;
-  }
-  function br() public view returns (address){
-    return address(_br);
   }
   function create_registry(string memory child_name, address parent) public returns (Registry) {
     Registry child=Registry(Clones.clone(base_registry)).init(child_name);
