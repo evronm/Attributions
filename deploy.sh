eee80ad46f1aa785db08bd7ffdbc8ca5200a8092
Mike Evron <evronm@gmail.com>
2022-05-13 23:15:59 -0700

    deploy script finally works

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ export KEY=0xf7d6fea62eeac6ceb372632ecc10702f4f97899d48647ddc88b0a31e438ad450
 export LIB_FILE='./src/Common.sol'
 export UTILS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY $LIB_FILE:Utils|grep "Deployed to:"|sed "s/Deployed to: //"`
 export UTILS_LIB="$LIB_FILE:Utils:${UTILS_ADDR}"
-export KVS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY --libraries $UTILS_LIB $LIB_FILE:Kvs|grep "Deployed to:"|sed "s/Deployed to: //"`
-export KVS_LIB="$LIB_FILE:Kvms:${KVS_ADDR}"
+export KVMS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY --libraries $UTILS_LIB $LIB_FILE:Kvs|grep "Deployed to:"|sed "s/Deployed to: //"`
+export KVMS_LIB="$LIB_FILE:Kvms:${KVMS_ADDR}"
 export STR_ADDRS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY --libraries $UTILS_LIB $LIB_FILE:Str_addrs|grep "Deployed to:"|sed "s/Deployed to: //"`
 export STR_ADDRS_LIB="$LIB_FILE:Str_addrs:${STR_ADDRS_ADDR}"
 export TAGS_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY --libraries $UTILS_LIB $LIB_FILE:Tags|grep "Deployed to:"|sed "s/Deployed to: //"`
 export TAGS_LIB="$LIB_FILE:Tags:${TAGS_ADDR}"
-export CONTRACT_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY --libraries $UTILS_LIB $KVMS_LIB $STR_ADDRS_LIB $TAGS_LIB ./src/Factory.sol:Factory|grep "Deployed to:"|sed "s/Deployed to: //"`
+export CONTRACT_ADDR=`forge create --legacy --rpc-url $RPC_URL --private-key $KEY ./src/Factory.sol:Factory --libraries $UTILS_LIB --libraries $KVMS_LIB --libraries $STR_ADDRS_LIB --libraries $TAGS_LIB |grep "Deployed to:"|sed "s/Deployed to: //"`
 cast send $CONTRACT_ADDR "init()" --rpc-url $RPC_URL --private-key $KEY --legacy
